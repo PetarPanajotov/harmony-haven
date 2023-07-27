01e635a4e193304919e52acc8bb1f4653f64e087
Petar Panayotov <dakona98@gmail.com>
2023-07-28 02:17:40 +0300

    Added jsonwebtoken library and  utils

---
+++ server/utils/jwtUtils.js
@@ -0,0 +1,7 @@
+const jwtCallback = require('jsonwebtoken');
+const util = require('util');
+
+module.exports = {
+    signToken: util.promisify(jwtCallback.sign),
+    verifyToken: util.promisify(jwtCallback.verify)
+};
