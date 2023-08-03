18cc8666b64cb1521193ca30c21e1fa5a929bf69
Petar Panayotov <dakona98@gmail.com>
2023-08-03 21:10:16 +0300

    fixed links

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
 
 
@@ -12,7 +13,8 @@ import { AuthenticateComponent } from './authenticate/authenticate.component';
     AuthenticateComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [FooterComponent, HeaderComponent, AuthenticateComponent]
 })
