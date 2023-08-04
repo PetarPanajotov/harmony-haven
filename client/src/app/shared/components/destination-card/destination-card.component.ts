3e970de24c214a0768dfa4df1c865bbd89be7f3e
Petar Panayotov <dakona98@gmail.com>
2023-08-04 16:45:07 +0300

    Seperated destination card into shared component.

---
+++ client/src/app/shared/components/destination-card/destination-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-card',
+  templateUrl: './destination-card.component.html',
+  styleUrls: ['./destination-card.component.css']
+})
+export class DestinationCardComponent {
+  @Input() destination: any;
+}
