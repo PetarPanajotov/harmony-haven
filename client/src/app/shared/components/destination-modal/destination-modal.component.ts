d2847d41bdc37d2d6ed60f1c159f63199b16d698
Petar Panayotov <dakona98@gmail.com>
2023-08-08 19:08:37 +0300

    Moved destination modal to shared

---
+++ client/src/app/shared/components/destination-modal/destination-modal.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { DestinationService } from 'src/app/core/services/destination.service';
+import { ModalService } from 'src/app/core/services/modal.service';
+
+
+@Component({
+  selector: 'app-destination-modal',
+  templateUrl: './destination-modal.component.html',
+  styleUrls: ['./destination-modal.component.css']
+})
+
+export class DestinationModalComponent implements OnInit {
+  @Input() isEditMode: boolean = false;
+  @Output() destinationAdded: EventEmitter<any> = new EventEmitter();
+  destinationForm:any = [];
+
+  ngOnInit(): void {
+    this.destinationForm = new FormGroup({
+      destinationName: new FormControl(''),
+      destinationLocation: new FormControl(''),
+      imgURL: new FormControl('')
+    }); 
+  }
+  constructor(private modalService: ModalService, private destinationService: DestinationService) { };
+
+
+  closeCreateModal(): void {
+    this.modalService.closeModal();
+  };
+
+  createDestination(): void {
+    const {
+      destinationName,
+      destinationLocation,
+      imgURL
+    } = this.destinationForm.value;
+
+    this.destinationService.createDestination(destinationName!, destinationLocation!, imgURL!)
+      .subscribe((destination) => {
+        this.destinationAdded.emit(destination)
+        this.modalService.closeModal();
+      });
+  };
+}
