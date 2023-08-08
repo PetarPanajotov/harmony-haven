03c093fe1a30321dcc7cb81d9facc6739c43b54c
Petar Panayotov <dakona98@gmail.com>
2023-08-08 19:08:55 +0300

    removed unnecessary paths

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,14 +5,12 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DestinationsComponent } from './pages/destinations/destinations.component';
 import { HotelsComponent } from './pages/hotels/hotels.component';
-import { CreateDestinationModalComponent } from './pages/create-destination-modal/create-destination-modal.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'destinations', component: DestinationsComponent},
-  {path: 'destinations/create', component: CreateDestinationModalComponent},
   {path: 'destinations/dubai', component: HotelsComponent}
 ];
 
