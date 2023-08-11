f11b5213b44bf64c46baa2017d148376c7aef8a9
Petar Panayotov <dakona98@gmail.com>
2023-08-11 18:00:35 +0300

    Update hotels.component.ts

--- client/src/app/pages/hotels/hotels.component.ts
+++ client/src/app/pages/hotels/hotels.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DestinationService } from 'src/app/core/services/destination.service';
-import { Destination } from 'src/app/core/types/destination';
 
 @Component({
   selector: 'app-hotels',
@@ -9,10 +8,10 @@ import { Destination } from 'src/app/core/types/destination';
   styleUrls: ['./hotels.component.css']
 })
 export class HotelsComponent implements OnInit {
-  destination: Destination | undefined;
-
-  constructor(private route: ActivatedRoute, private destinationService: DestinationService) { }
+  destination = this.destinationService.destinationData;
   
+  constructor(private route: ActivatedRoute, private destinationService: DestinationService) { }
+
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const id: string | null = params.get('id');
