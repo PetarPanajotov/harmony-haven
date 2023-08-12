1d694373f81d0d70322bc29ec76e4fffcf26268b
Petar Panayotov <dakona98@gmail.com>
2023-08-12 17:51:36 +0300

    Update destination.ts

--- client/src/app/core/types/destination.ts
+++ client/src/app/core/types/destination.ts
@@ -3,6 +3,7 @@ export interface Destination {
     destinationName: string,
     destinationLocation: string,
     imgURL: string,
+    hotels?: any,
     offersCount?: number,
     __v?: number,
     popular?: boolean
