de0696eae73923d9342525e95d9df02261a2d1e6
Petar Panayotov <dakona98@gmail.com>
2023-08-12 22:56:21 +0300

    Update popular-filter.pipe.ts

--- client/src/app/shared/pipes/popular-filter.pipe.ts
+++ client/src/app/shared/pipes/popular-filter.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PopularFilterPipe implements PipeTransform {
 
   transform(destinations: any[], popular: boolean = true): any[] {
+    if (!destinations || destinations.length < 0) {
+      return destinations
+    }
     if (popular === true) {
       return destinations.filter(destination => destination.popular === popular);
     } else {
