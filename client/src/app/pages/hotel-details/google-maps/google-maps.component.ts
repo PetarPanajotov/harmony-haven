5c8d40b5996ab276eacb418d3f4da440dc3781ca
Petar Panayotov <dakona98@gmail.com>
2023-08-13 18:45:39 +0300

    added google maps, as its for now I wont use it.

---
+++ client/src/app/pages/hotel-details/google-maps/google-maps.component.ts
@@ -0,0 +1,17 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-google-maps',
+//   templateUrl: './google-maps.component.html',
+//   styleUrls: ['./google-maps.component.css']
+// })
+// export class GoogleMapsComponent {
+//   center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
+//   zoom = 4;
+//   markerOptions: google.maps.MarkerOptions = {draggable: false};
+//   markerPositions: google.maps.LatLngLiteral[] = [];
+
+//   addMarker(event: google.maps.MapMouseEvent) {
+//     this.markerPositions.push(event.latLng!.toJSON());
+//   }
+// }
