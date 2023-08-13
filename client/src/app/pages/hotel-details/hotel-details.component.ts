5dafb35d403affda4467a22c48943e4e370ec0e9
Petar Panayotov <dakona98@gmail.com>
2023-08-13 18:46:04 +0300

    Added hotel-details styles.

--- client/src/app/pages/hotel-details/hotel-details.component.ts
+++ client/src/app/pages/hotel-details/hotel-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-hotel-details',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./hotel-details.component.css']
 })
 export class HotelDetailsComponent {
+  autoExpand(event: any) {
+    const textarea = event.target;
+    textarea!.style.height = 'auto';
+    textarea!.style.height = (textarea.scrollHeight) + 'px'
+  }
+  // apiLoaded: Observable<boolean>;
+  // constructor(httpClient: HttpClient) {
+  //   this.apiLoaded = httpClient.jsonp(`https://maps.googleapis.com/maps/api/js?key=AIzaSyAJrAgwcc1vJhAV0nh4B0j0IjyZNQlXAPA`, 'callback')
+  //     .pipe(
+  //       map(() => true),
+  //       catchError(() => of(false)),
+  //     );
+  // }
 
 }
