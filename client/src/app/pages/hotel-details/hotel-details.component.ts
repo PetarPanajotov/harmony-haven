e3ee7702c2ffa10780df42369342c5949facb851
Petar Panayotov <dakona98@gmail.com>
2023-08-14 16:34:57 +0300

    Update hotel-details.component.ts

--- client/src/app/pages/hotel-details/hotel-details.component.ts
+++ client/src/app/pages/hotel-details/hotel-details.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DestinationService } from 'src/app/core/services/destination.service';
 
 @Component({
   selector: 'app-hotel-details',
   templateUrl: './hotel-details.component.html',
   styleUrls: ['./hotel-details.component.css']
 })
-export class HotelDetailsComponent {
+export class HotelDetailsComponent implements OnInit{
+  hotel:any;
+
+  constructor(private destinationService: DestinationService, private route: ActivatedRoute){}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const hotelId: string | null = params.get('hotelId');
+    this.destinationService.getHotelById(hotelId!)
+      .subscribe((hotel) => console.log(hotel))
+  })
+}
   autoExpand(event: any) {
     const textarea = event.target;
     textarea!.style.height = 'auto';
