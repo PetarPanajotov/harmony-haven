803fbd651f4798bcb3b1adba01567e8e6dec5df8
Petar Panayotov <dakona98@gmail.com>
2023-08-15 16:48:21 +0300

    Hotel service doesnt include reviews.

--- server/services/hotelService.js
+++ server/services/hotelService.js
@@ -1,19 +1,34 @@
 const { findDestinationById, findHotelById } = require("../utils/dbFunctionsUtils")
-const Hotel = require("../models/hotelModel")
+const Hotel = require("../models/hotelModel");
+const Review = require("../models/reviewModel");
 
 exports.createHotel = async (id, hotelName, hotelLocation, imgURL, stars, type, price, freeRooms, description) => {
    const destination = await findDestinationById(id);
-   const newHotel = new Hotel({hotelName, hotelLocation, imgURL, stars, type, price, freeRooms, description});
+   const newHotel = new Hotel({ hotelName, hotelLocation, imgURL, stars, type, price, freeRooms, description });
    await newHotel.save();
    destination.hotels.push(newHotel._id);
    await destination.save();
    return newHotel;
 };
+
 exports.hotels = async (destinationId) => {
-   const destination = await findDestinationById(destinationId).populate('hotels');
+   const destination = await findDestinationById(destinationId).populate({
+       path: 'hotels',
+       populate: {
+           path: 'reviews'
+       }
+   })
+   //as the idea is to not include reviews, since they are not shown in the page. Remove this piece of code below, if you want to include it.
+   .then((destination) => destination.toObject());
+
+   destination.hotels.forEach(hotel => {
+       hotel.reviews = undefined;
+   });
+
    return destination;
 };
-exports.findHotel = async(hotelId) => {
+
+exports.findHotel = async (hotelId) => {
    const oneHotel = await findHotelById(hotelId);
    return oneHotel;
 };
