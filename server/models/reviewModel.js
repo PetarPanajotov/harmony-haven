401d0c10e8c6410f5ee29af5befeb208c8155c1d
Petar Panayotov <dakona98@gmail.com>
2023-08-15 16:46:36 +0300

    added review model.

---
+++ server/models/reviewModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types
+
+const reviewSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    _ownerId: [{
+        type: ObjectId,
+        ref: 'User'
+    }]
+})
+module.exports = mongoose.model('Review', reviewSchema)
