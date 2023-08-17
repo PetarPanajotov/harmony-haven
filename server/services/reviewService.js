fbf116bb745d7ac938ada5460723b4198dc3494e
Petar Panayotov <dakona98@gmail.com>
2023-08-17 13:32:49 +0300

    Update reviewService.js

--- server/services/reviewService.js
+++ server/services/reviewService.js
@@ -3,6 +3,7 @@ const { findHotelByIdAndUpdate } = require("../utils/dbFunctionsUtils");
 
 exports.createReview = async (hotelId, rating, text, userId) => {
     const newReview = new Review({rating, text, _ownerId: userId});
-    await newReview.save().then(async(review) => await findHotelByIdAndUpdate({_id: hotelId}, {$push: {reviews: review._id}}));
+    await newReview.save();
+    await findHotelByIdAndUpdate({_id: hotelId}, {$push: {reviews: newReview._id}});
     return newReview;
  };
