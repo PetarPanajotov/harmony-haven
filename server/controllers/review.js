ae635ef7167ceb8fd2d03885db8671ee78825c3d
Petar Panayotov <dakona98@gmail.com>
2023-08-17 15:25:13 +0300

    Update review.js

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -1,17 +1,30 @@
-const { createReview } = require("../services/reviewService");
+const { createReview, reviews } = require("../services/reviewService");
 const { ErrorMessage } = require("../utils/errorHandlerUtils");
 
 exports.postReview = async(req, res) => {
     const { id } = req.params;
-    const { _id: userId} = req.user
+    const { _id: userId} = req.user;
 
     const {rating, text} = req.body;
     try {
        const data = await createReview(id, rating, text, userId);
        res.status(201)
-        .send(data)
+        .send(data);
     } catch(error) {
         res.status(400)
         .send(ErrorMessage(error));
-    }
-}
+    };
+};
+
+exports.getReviews = async(req, res) => {
+    const { id } = req.params;
+    const { offset, limit } = req.query;
+    try {
+        const data = await reviews(id, offset, limit);
+        res.status(200)
+            .send(data);
+    } catch(error) {
+        res.status(404)
+            .send(ErrorMessage(error));
+    };
+};
