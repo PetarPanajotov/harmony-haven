97e73fef1fe10096eec70c57ca032c0ddab999b2
Petar Panayotov <dakona98@gmail.com>
2023-08-17 21:01:47 +0300

    added validators

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/core/services/user.service';
-
+import { ValidatorService } from 'src/app/core/services/validator.service';
 
 @Component({
     selector: 'app-register',
@@ -11,18 +11,22 @@ import { UserService } from 'src/app/core/services/user.service';
 })
 export class RegisterComponent {
     profileForm = new FormGroup({
-        email: new FormControl(''),
-        firstName: new FormControl(''),
-        lastName: new FormControl(''),
-        password: new FormControl(''),
-        repeatPassword: new FormControl('')
+        email: new FormControl('', [Validators.required, this.validatorService.customEmailValidator()]),
+        firstName: new FormControl('', [Validators.required, this.validatorService.customFirstNameValidator()]),
+        lastName: new FormControl('', [Validators.required, this.validatorService.customLastNameValidator()]),
+        password: new FormControl('', [Validators.minLength(5)]),
+        repeatPassword: new FormControl('', [this.validatorService.matchPasswordValidator('password')])
     });
     constructor(
         private userService: UserService,
-        private router: Router
+        private router: Router,
+        private validatorService: ValidatorService
     ) {}
 
     register(): void {
+        if(this.profileForm.invalid) {
+            return
+        };
         const {
             email,
             firstName,
@@ -30,6 +34,7 @@ export class RegisterComponent {
             password,
             repeatPassword
         } = this.profileForm.value;
+        console.log(email)
         this.userService.register(email!, firstName!, lastName!, password!, repeatPassword!)
             .subscribe(() => this.router.navigate(['/']));
     };
