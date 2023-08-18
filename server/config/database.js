606b9cbd45fd3ef55df1d793f7ae2ffcdbe13661
Petar Panayotov <dakona98@gmail.com>
2023-08-18 03:54:01 +0300

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 async function databaseInit() {
     mongoose.set('strictQuery', false);
     try {
+        console.log(config.databaseURL)
         await mongoose.connect(config.databaseURL);
         return console.log('Database is connected');
     } catch(error) {
