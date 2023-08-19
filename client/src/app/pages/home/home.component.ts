525942e01956b4fb0e959c9db9ba6d1c2f11aa11
Petar Panayotov <dakona98@gmail.com>
2023-08-19 16:53:57 +0300

    Implement home page as now correct shows destination cards

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DestinationService } from 'src/app/core/services/destination.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  destinationList: any = []
+  pagination = {
+    offset: 0,
+    limit: 4
+  };
 
-}
+  constructor(private destinationService: DestinationService) {}
+
+  ngOnInit(): void {
+    this.destinationService.getDestinations(undefined, this.pagination.offset, this.pagination.limit)
+      .subscribe((destinationData) => this.destinationList = destinationData);
+  };
+};
