1a7dfb1d8ab9cd57e3ef2ab7cb163a9be18eedb0
Petar Panayotov <dakona98@gmail.com>
2023-08-19 17:01:23 +0300

    Updated database config

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 async function databaseInit() {
     mongoose.set('strictQuery', false);
-    console.log(process.env.DB_URL_CLOUD)
     try {
         await mongoose.connect(config.databaseURL);
         return console.log('Database is connected');
